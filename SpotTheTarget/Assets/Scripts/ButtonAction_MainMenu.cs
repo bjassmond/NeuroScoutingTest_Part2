4497941cff8f1f934cdc80dff95f57a0d89f5f1c
bjassmond <bjassmond@alum.wpi.edu>
2014-08-09 19:58:00 -0400

    Added Main Menu Button
    Added a main menu button action and placed the button on the Results
    screen.

---
+++ SpotTheTarget/Assets/Scripts/ButtonAction_MainMenu.cs
@@ -0,0 +1,43 @@
+﻿/* Module		: ButtonAction_MainMenu.cs
+ * Author		: Bryce Jassmond
+ * Email		: bjassmond@alum.wpi.edu
+ * CoAuthor		: N/A
+ * Email		: N/A
+ *
+ * Description	: Go to the main menu
+ *
+ * Date			: 2014/08/09
+ *
+ * History:
+ * Revision		Date			Changed By
+ * --------		----------		----------
+ * 01.00		2014/08/09		bjassmond
+ *
+ */
+
+/* -- Using ----------------------------------------------------------------- */
+using UnityEngine;
+using System.Collections;
+
+/* -- Class Definition ------------------------------------------------------ */
+public class ButtonAction_MainMenu : ButtonAction {
+	
+	/* -- Variables --------------------------------------------------------- */
+	
+	
+	/* -- Methods ----------------------------------------------------------- */
+	
+	
+	/* Method		: public override void runAction()
+	 *
+	 * Description	: Overrides abstract action definition. Switch to main menu
+	 *
+	 * Parameters	: void
+	 *
+	 * Returns		: void
+	 */
+	public override void runAction() {
+		Application.LoadLevel("MainMenu");
+	}
+	
+}
